63c4cc6796a632b4293d3f3fab52b8d68f2c50d4
jamie3221 <jamie.kariuki@strathmore.edu>
2025-05-20 14:44:45 +0300

    commit 1 (Jamie . K)

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    private bool canMove = true;
+
+    void Update()
+    {
+        if (!canMove) return;
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveX, 0, moveZ);
+        transform.translate(move * speed * Time.deltaTime, Space.World);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            canMove = false;
+            Debug.Log("Player hit an obstacle!");
+        }
+    }
+}
